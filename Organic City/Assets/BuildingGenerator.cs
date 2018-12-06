1be8087396e26f357e7e23ca93d761c42071f983
Ryan Higgins <c16381886@mydit.ie>
2018-12-06 14:39:56 +0000

    Switched more to game of life grid system than freeform

---
+++ Organic City/Assets/BuildingGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGenerator : MonoBehaviour
+{
+	public int buildingLimit;
+	public GameObject buildingPrefab;
+
+	private int buildingCount;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < buildingLimit; i++)
+		{
+			GameObject buildingPrefabClone = buildingPrefab;
+			Instantiate(buildingPrefabClone, new Vector3((int) Random.Range(-50, 50), 5, (int) Random.Range(50, -50)),
+				buildingPrefabClone.transform.rotation);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Organic City/Assets/GridBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBuilder : MonoBehaviour
+{
+
+	public int rowCount;
+	public int colCount;
+	public GameObject buildingPrefab;
+	public GameObject roadPrefab;
+	
+	private int cellWidth;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		cellWidth = 100 / rowCount;
+		bool[,] cityGrid = new bool[rowCount,colCount];
+
+		for (int i = 0; i < rowCount; i++)
+		{
+			for (int j = 0; j < colCount; j++)
+			{
+				if (Random.Range(0,2) == 1)
+				{
+					cityGrid[i, j] = true;
+				}
+				else
+				{
+					cityGrid[i, j] = false;
+				}
+
+				if (cityGrid[i, j])
+				{
+					Instantiate(buildingPrefab,new Vector3(gameObject.transform.position.x + j * cellWidth, 5, gameObject.transform.position.z - i * cellWidth), gameObject.transform.rotation);
+				}
+				else
+				{
+					Instantiate(roadPrefab,new Vector3(gameObject.transform.position.x + j * cellWidth, 0, gameObject.transform.position.z - i * cellWidth), gameObject.transform.rotation);
+				}
+			}
+		}
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
