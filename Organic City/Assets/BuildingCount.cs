652ebd297721d21a5cbd732e57b01d9c6df00401
Ryan Higgins <c16381886@mydit.ie>
2018-12-15 20:16:48 +0000

    More Stuff

---
+++ Organic City/Assets/BuildingCount.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using UnityEngine;
+
+public class BuildingCount : MonoBehaviour
+{
+
+	public bool hasAdded;
+	public List<GameObject> otherBuildings;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		hasAdded = false;
+		otherBuildings = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (!hasAdded)
+		{
+			if (other.gameObject.tag == "Building")
+			{
+				otherBuildings.Add(other.gameObject);
+				hasAdded = true;
+			}
+		}
+	}
+}
